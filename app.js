118e9c56825924a0cee34315904c6e327345d6c3
Allison01~ <farrella95@gmail.com>
2023-06-07 19:05:49 -0400

    style stuff

---
+++ app.js
@@ -0,0 +1,7 @@
+//select elemments
+let dFour = document.querySelector('.d-four');
+let dSix = document.querySelector('.d-six');
+let dEight = document.querySelector('.d-eight');
+let dTen = document.querySelector('.d-ten');
+let dTwelve = document.querySelector('.d-twelve');
+let dTwenty = document.querySelector('.d-twenty');
